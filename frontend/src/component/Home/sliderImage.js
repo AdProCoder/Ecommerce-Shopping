61ef6045aa7b78674fff940ff988f7c85953155e
Ashutosh Dash <68074290+AdProCoder@users.noreply.github.com>
2022-05-14 00:10:23 +0530

    Add files via upload

---
+++ frontend/src/component/Home/sliderImage.js
@@ -0,0 +1,30 @@
+
+import Second from "./h1.jpg";
+import Third from "./h2.png";
+import Four from "./h3.jpg";
+import Five from "./h4.jpg";
+
+export default [
+  {
+    title: "",
+    description: "",
+    urls: Third,
+  },
+  {
+    title:"Welcome to Ecommerce",
+    description: "All available at your doorstep",
+    urls: Four,
+  },
+  { 
+
+    title: "",
+    description: "",
+    urls: Second,
+  },
+  {
+    title:"Welcome to Ecommerce",
+    description: "Shop the most attractive products here",
+    urls: Five,
+  },
+
+];
